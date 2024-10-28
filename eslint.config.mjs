2c6c58465cf8d08459b37c01323513fe1bed8a6e
ramfdp <elramaferdian@gmail.com>
2024-10-28 15:11:21 +0700

    eslint adding

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  daStyle,
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
